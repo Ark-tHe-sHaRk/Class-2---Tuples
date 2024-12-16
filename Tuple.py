14242af607c8b3821c5a41d3436b7ce6ebcca037
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2024-12-16 17:11:08 +0000

    Tuple_Operations

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+#Creating a tuple
+Tuple = ("numbers", 1, 2, 3, 4, 5, 0.8, "Ark is my name", True)
+print(Tuple)
+Tuple_1 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+#Adding an Int Value
+Tuple_3 = Tuple_1 + (0,)
+print(Tuple_3)
+#Slicing operator
+Slicing = (1, 2, 3, 4, 5, "Hello")
+print(Slicing[1:5])
+#Number of occurances
+Occuring = (10, 10, 20, 30, 10)
+print(Occuring.count(10))
